8f38d9b003e2c5be68574f352eb8462f3827bb6f
irineufreitas <freitaschoro@gmail.com>
2024-03-05 12:24:23 +0100

    program 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,51 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is your grade percentage?");
+        string gradeMan = Console.ReadLine();
+        int gradeRock = int.Parse(gradeMan);
+
+        string letter = "";
+
+        if (gradeRock >= 90)
+        {
+            letter = "A";
+            
+        }
+
+        else if (gradeRock >= 80)
+        {
+            letter = "B";
+            
+        }
+
+        else if (gradeRock >= 70)
+        {
+            letter = "C";
+            
+        }
+
+        else if (gradeRock >=60)
+        {
+            letter = "D";
+            
+        }
+
+        else if (gradeRock < 60)
+        {
+            letter = "F";
+            
+        }
+
+        if (gradeRock >= 70)
+        {
+            Console.Write($"Congratulations, Your grade is {letter} and you have passed the course!");
+            
+        }
+        else
+        {
+            Console.WriteLine("You have not passed this time, keep working to pass next time!");
+        }
+
     }
 }
