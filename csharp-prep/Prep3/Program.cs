d19861d9ddfaa7f42063fb783b08c72839119014
irineufreitas <freitaschoro@gmail.com>
2024-03-05 16:22:29 +0100

    prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,35 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        // Console.Write("What is the magic number?");
+        // string magicNumber = Console.ReadLine();
+        // int storeNumber = int.Parse(magicNumber);
+
+        Random randomGenerator = new Random();
+        int storeNumber = randomGenerator.Next(1, 100);
+        
+        int storeGuess = 0;
+        
+
+        while(storeGuess != storeNumber)
+        {   
+            Console.Write("What is your guess?");
+            string guessNumber = Console.ReadLine();
+            storeGuess = int.Parse(guessNumber);
+    
+            if (storeGuess > storeNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            
+            if (storeGuess < storeNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            if (storeGuess == storeNumber)
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        }
     }
 }
