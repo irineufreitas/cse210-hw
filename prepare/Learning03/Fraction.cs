a75008751d41ce6639b5b3a17423d1b986e55033
irineufreitas <freitaschoro@gmail.com>
2024-05-11 18:38:45 +0200

    learning3

---
+++ prepare/Learning03/Fraction.cs
@@ -0,0 +1,57 @@
+using System;
+
+public class Fraction
+        {
+            private int _top;
+            private int _bottom;
+
+            public Fraction()
+            {
+                _top = 1;
+                _bottom = 1;
+            }
+
+            public Fraction(int wholeNumber)
+            {
+                _top = wholeNumber;
+                _bottom = 1;
+            }
+
+            public Fraction(int top, int bottom)
+            {
+                _top = top;
+                _bottom = bottom;
+            }
+
+            public int GetTop()
+            {
+                return _top;
+            }
+            public void SetTop(int top)
+            {
+                _top = top;
+                
+            }
+            public int GetBottom()
+            {
+                return _bottom;
+            }
+            public void SetBottom(int bottom)
+            {
+                _bottom = bottom;
+            }
+
+            public string GetFractionString()
+            {
+                string top1 = Convert.ToString(_top);
+                string bottom1 = Convert.ToString(_bottom);
+
+                return $"{top1}/{bottom1}";
+                
+            }
+
+            public double GetDecimalValue()
+            {
+                return(double) _top/(double)_bottom;
+            }
+        }
