3b2864d29c3532966aecae4dd1c0cef815c056d4
irineufreitas <freitaschoro@gmail.com>
2024-05-26 03:07:30 +0200

    shapes

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -1,9 +1,35 @@
 using System;
+using System.Drawing;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        
+
+        Square square = new Square("Black", 5);
+        Console.WriteLine(square.GetArea());
+
+        Console.WriteLine(square.GetColor());
+
+        Rectangle rectangle = new Rectangle("white", 3, 4);
+        Console.WriteLine(rectangle.GetArea());
+
+        Circle circle = new Circle("Yellow", 6.5);
+        Console.WriteLine(circle.GetArea());
+
+        List<Shape> shapes = new List<Shape>();
+        shapes.Add(new Square("Purple", 3));
+        shapes.Add(new Rectangle ("Pink", 3, 7));
+        shapes.Add(new Circle("Blue", 4));
+
+        foreach (Shape shape in shapes)
+        {
+            double area = shape.GetArea();
+            string color = shape.GetColor();
+            Console.WriteLine($"{color} - {area}");
+        }
+
+
     }
 }
